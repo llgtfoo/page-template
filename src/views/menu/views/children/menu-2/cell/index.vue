bc72c7fd84058a471772e63ad0503df9e2fcf768
前端小丑 <lltfoo@163.com>
2021-03-22 17:11:15 +0800

    tijiao

--- src/views/menu/views/children/menu-2/cell/index.vue
+++ src/views/menu/views/children/menu-2/cell/index.vue
@@ -1,11 +1,46 @@
 <template>
-  <div>
-    ceshi2
+  <div class="echarts-demo">
+    <echart-common :options="options"></echart-common>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      options: {
+        tooltip: {},
+        grid: {
+          left: '5%',
+          right: '5%',
+          bottom: '5%',
+          top: '5%',
+        },
+        xAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        },
+        yAxis: {
+          type: 'value',
+        },
+        series: [{
+          data: [120, 200, 150, 80, 70, 110, 130],
+          type: 'bar',
+          showBackground: false,
+          backgroundStyle: {
+            color: 'rgba(180, 180, 180, 0.2)',
+          },
+        }],
+      },
+    }
+  },
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped lang='scss'>
+.echarts-demo {
+  width: 100%;
+  height: 100%;
+  text-align: center;
+}
+</style>
