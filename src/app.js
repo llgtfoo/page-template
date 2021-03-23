8e46bd1bfe91f7916928a14539f7bfcb17b5f98f
前端小丑 <lltfoo@163.com>
2021-03-23 10:37:34 +0800

    增加示例

--- src/app.js
+++ src/app.js
@@ -1,18 +1,23 @@
+import '@/assets/css/init.css' //默认css
+import '@/assets/css/scroll.scss' //默认滚动条css
+import '@/lib/http/axios' //axios
+import { http } from '@/lib/http/index' //封装后的axios
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+import * as echarts from 'echarts'
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import { sync } from 'vuex-router-sync'
-import views from './views/index'
-import '@/lib/http/axios'//axios
-import { http } from '@/lib/http/index'//封装后的axios
-
-import components from './components/index'
-import directives from './directives/index'
-import utils from './utils/index'
-import echarts from 'echarts'
 // 设置cookie
 import VueCookies from 'vue-cookies'
+import Fragment from 'vue-fragment'
+import { sync } from 'vuex-router-sync'
+import App from './App.vue'
+import components from './components/index'
+import directives from './directives/index'
+import router from './router'
+import store from './store'
+import utils from './utils/index'
+import views from './views/index'
+
 Vue.use(VueCookies)
 
 Vue.use(components)//全局组件
@@ -21,19 +26,14 @@ Vue.use(utils)//全局工具函数
 // Vue.use(Http)//this.http
 sync(store, router)//route和store结合
 
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css';
-Vue.use(Antd);//ui框架
+Vue.use(Antd)//ui框架
 
-import Fragment from 'vue-fragment'
 Vue.use(Fragment.Plugin)//跟元素
 
 Vue.prototype.http = http
 Vue.prototype.echarts = echarts
 Vue.use(views, router, store)
 
-import '@/assets/css/init.css'//默认css
-import '@/assets/css/scroll.scss'//默认滚动条css
 Vue.config.productionTip = false
 export default new Vue({
   router,
--- src/views/menu/views/children/menu-2/cell/index.vue
+++ src/views/menu/views/children/menu-2/cell/index.vue
@@ -9,7 +9,12 @@ export default {
   data() {
     return {
       options: {
-        tooltip: {},
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: { // 坐标轴指示器，坐标轴触发有效
+            type: 'shadow', // 默认为直线，可选为：'line' | 'shadow'
+          },
+        },
         grid: {
           left: '5%',
           right: '5%',
